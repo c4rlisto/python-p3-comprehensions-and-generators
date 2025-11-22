21b770500d725bbb8f4b838c9d1b760d920a7c23
Carl Kiprono <carlkiprono00@gmail.com>
2025-11-22 10:03:33 +0300

    initial commit

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
-#!/usr/bin/env python3
+def return_evens(sequence):
+    """
+    Return a list of all even numbers in the sequence.
+    Uses a list comprehension.
+    """
+    return [n for n in sequence if n % 2 == 0]
 
-def return_evens(num_list):
-    pass
 
-def make_exclamation(sentence_list):
-    pass
+def make_exclamation(sentences):
+    """
+    Return a list of sentences with an exclamation mark added.
+    Uses a list comprehension.
+    """
+    return [sentence + "!" for sentence in sentences]
+
